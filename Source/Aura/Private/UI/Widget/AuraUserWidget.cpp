5e63c956e968a23ffa6afe2a2125682c781bcb98
KristianBakov <kristian.bakov@yahoo.com>
2024-02-02 20:34:28 +0000

    Adding MVC setup

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Kristian Bakov 2024
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Kristian Bakov 2024
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
