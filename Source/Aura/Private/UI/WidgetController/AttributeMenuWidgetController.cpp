bd8d483976a84dba53a76cf7299648c58a5050ab
KristianBakov <kristian.bakov@yahoo.com>
2024-02-08 22:02:50 +0000

    Created attribute widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Kristian Bakov 2024
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
